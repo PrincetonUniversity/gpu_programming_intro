951dfdd088d65723c0c2607e79afe11bf020b7ba
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-06 10:41:49 -0500

    Update svd_cupy.py

--- 03_your_first_gpu_job/cupy/svd_cupy.py
+++ 03_your_first_gpu_job/cupy/svd_cupy.py
@@ -4,5 +4,7 @@ import cupy as cp
 N = 1000
 X = cp.random.randn(N, N, dtype=np.float64)
 u, s, v = cp.linalg.decomposition.svd(X)
+cp.cuda.Device(0).synchronize()
+
 print(cp.asnumpy(s).sum())
 print("CuPy version: ", cp.__version__)
