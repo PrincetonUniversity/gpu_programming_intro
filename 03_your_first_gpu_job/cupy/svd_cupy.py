f8172ab0e798e81e0a577e802a8dd1f876518c96
jdh4 <52128661+jdh4@users.noreply.github.com>
2019-11-11 15:43:16 -0500

    Update svd_cupy.py

--- 03_your_first_gpu_job/cupy/svd_cupy.py
+++ 03_your_first_gpu_job/cupy/svd_cupy.py
@@ -1,10 +1,14 @@
+from time import perf_counter
 import numpy as np
 import cupy as cp
 
 N = 1000
 X = cp.random.randn(N, N, dtype=np.float64)
+t0 = perf_counter()
 u, s, v = cp.linalg.decomposition.svd(X)
 cp.cuda.Device(0).synchronize()
+elapsed_time = perf_counter() - t0
 
+print("Execution time: ", elapsed_time)
 print(cp.asnumpy(s).sum())
 print("CuPy version: ", cp.__version__)
