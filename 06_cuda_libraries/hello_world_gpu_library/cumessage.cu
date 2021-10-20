09881e05c48ae7b655a765c99d93a37c8573d655
jdh4 <52128661+jdh4@users.noreply.github.com>
2021-10-19 20:29:01 -0400

    Create cumessage.cu

---
+++ 06_cuda_libraries/hello_world_gpu_library/cumessage.cu
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "cumessage.h"
+
+__global__ void GPUFunction_kernel() {
+  printf("Hello world from the GPU.\n");
+}
+
+void GPUFunction() {
+  GPUFunction_kernel<<<1,1>>>();
+  cudaDeviceSynchronize();
+}
